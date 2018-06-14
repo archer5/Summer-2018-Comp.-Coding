1d775ef69fac6936397ad41a7dce11cbccce90b9
archer5 <matthew.buckley@hotmail.co.uk>
2018-06-14 13:00:48 +0100

    Processing methods written, functional for given test cases

--- src/Problem_Set_1_Dynamic_Programming/Boredom.java
+++ src/Problem_Set_1_Dynamic_Programming/Boredom.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Boredom {
 
@@ -12,12 +13,17 @@ public class Boredom {
     private String sequence[] = new String[100000]; //array of sequence as String elements
     private int numInt; //number of elements in sequence as an int
     private int sequenceInts[] = new int[100000]; //array of sequence as int elements
+    private HashMap<Integer, Integer> hashMap = new HashMap<>(); //counts number of occurrences of each value in sequence
+    private int score; //output score
 
     public static void main(String[] args) {
 
         Boredom boredom = new Boredom();
         boredom.input();
         boredom.numConversion();
+        boredom.hashMap();
+        boredom.processing(boredom.findGreatestValue());
+        boredom.output();
 
     }
 
@@ -82,6 +88,96 @@ public class Boredom {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * Creates a HashMap storing all the values in the sequence and the number of times they occur
+     */
+    private void hashMap() {
+
+        for (int i = 0; i < sequence.length && !(sequence[i] == null); i++) {
+
+            /*
+            If already have a hash key for value, increment value (number of occurrences) by 1
+             */
+            if (hashMap.containsKey(sequenceInts[i])) {
+                hashMap.replace(sequenceInts[i], hashMap.get(sequenceInts[i]) + 1);
+            }
+            /*
+            Else add value to hashMap
+             */
+            else {
+                hashMap.put(sequenceInts[i], 1);
+            }
+
+        }
+
+    }
+
+    /**
+     * Finds the greatest value in the sequence from which to work backwards from
+     * @return
+     *          Returns greatest value
+     */
+    private int findGreatestValue() {
+
+        int greatestValue = 0; //will cause instant return of processing() with this initial value
+
+        try {
+
+            greatestValue = sequenceInts[0];
+            for (int i = 1; i < sequence.length && !(sequence[i] == null); i++) {
+                if (sequenceInts[i] > greatestValue) {
+                    greatestValue = sequenceInts[i];
+                }
+            }
+
+        } catch (NullPointerException e) {
+            System.out.println("Invalid Sequence Entered");
+            e.printStackTrace();
+        }
+
+        return greatestValue;
+
+    }
+
+    /**
+     * Processes and finds score
+     * @param greatestValue
+     *          Greatest value in sequence still to be processed
+     */
+    private void processing(int greatestValue) {
+
+        if (greatestValue == 0) {
+            return;
+        }
+
+        try {
+
+            /*
+            Calculates which would score more points - deleting the greatest value or the ones around it
+             */
+            if ((greatestValue - 1) * (hashMap.get(greatestValue - 1)) > (greatestValue * hashMap.get(greatestValue))) {
+                this.processing(greatestValue - 1);
+            }
+            else {
+                score += greatestValue * hashMap.get(greatestValue);
+                hashMap.remove(greatestValue - 1);
+                this.processing(greatestValue - 2);
+            }
+
+        } catch (NullPointerException e) { //in this case, greatestValue - 1 or greatestValue does not exist in the sequence, so hashMap.get() returns null
+            score += greatestValue * hashMap.get(greatestValue);
+            this.processing(greatestValue - 1);
+        }
+
+    }
+
+    /**
+     * Prints out score
+     */
+    private void output() {
+        System.out.println(score);
     }
 
 }
